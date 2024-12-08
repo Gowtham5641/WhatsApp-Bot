6f6956b3376a120f41c32a96069c31e465479d5c
Gowtham5641 <94177637+Gowtham5641@users.noreply.github.com>
2024-12-09 00:32:53 +0530

    Delete preinstall.sh

--- preinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Processing Aptfile..."
-while IFS= read -r package; do
-    echo "Trying to install $package..."
-    if ! apt-get install -y --no-install-recommends $package; then
-        echo "Skipping $package as it could not be installed."
-    fi
-done < Aptfile
